62647ddabea24829e3ca7c7d0dcc4935390d2cf7
Ilya <ilya03449@gmail.com>
2020-11-24 17:53:58 +0200

    created model Review. Implemented csv line parsing and tests

---
+++ src/main/java/com/test/sb/service/CsvLineParserImpl.java
@@ -0,0 +1,46 @@
+package com.test.sb.service;
+
+import com.opencsv.CSVParser;
+import com.test.sb.model.Review;
+import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CsvLineParserImpl implements CsvLineParser<Review> {
+    private static final int ID_INDEX = 0;
+    private static final int PRODUCT_ID_INDEX = 1;
+    private static final int USER_ID_INDEX = 2;
+    private static final int PROFILE_NAME_INDEX = 3;
+    private static final int NUMERATOR_INDEX = 4;
+    private static final int DENOMINATOR_INDEX = 5;
+    private static final int SCORE_INDEX = 6;
+    private static final int TIME_INDEX = 7;
+    private static final int SUMMARY_INDEX = 8;
+    private static final int TEXT_INDEX = 9;
+
+    @Override
+    public Review parseData(String line) {
+        CSVParser parser = new CSVParser();
+        try {
+            String[] values = parser.parseLine(line);
+            Review review = new Review();
+            review.setId(Long.parseLong(values[ID_INDEX]));
+            review.setProductId(values[PRODUCT_ID_INDEX]);
+            review.setUserId(values[USER_ID_INDEX]);
+            review.setProfileName(values[PROFILE_NAME_INDEX]);
+            review.setHelpfulnessNumerator(Integer.parseInt(values[NUMERATOR_INDEX]));
+            review.setHelpfulnessDenominator(Integer.parseInt(values[DENOMINATOR_INDEX]));
+            review.setScore(Integer.parseInt(values[SCORE_INDEX]));
+            review.setTime(LocalDateTime.ofInstant(Instant.ofEpochSecond(
+                    Long.parseLong(values[TIME_INDEX])), ZoneId.systemDefault()));
+            review.setSummary(values[SUMMARY_INDEX]);
+            review.setText(values[TEXT_INDEX]);
+            return review;
+        } catch (IOException e) {
+            throw new RuntimeException("Exception while parsing line: " + line, e);
+        }
+    }
+}
---
+++ src/test/java/com/test/sb/service/CsvLineParserImplTest.java
@@ -0,0 +1,66 @@
+package com.test.sb.service;
+
+import com.test.sb.model.Review;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class CsvLineParserImplTest {
+    public static final long ID = 1L;
+    public static final String PRODUCT_ID = "B001E4KFG0";
+    public static final String USER_ID = "A3SGXH7AUHU8GW";
+    public static final String PROFILE_NAME = "delmartian";
+    public static final int HELPFULNESS_NUMERATOR = 1;
+    public static final int HELPFULNESS_DENOMINATOR = 1;
+    public static final int SCORE = 5;
+    public static final long SECONDS = 1303862400L;
+    public static final String WRONG_FORMAT_LINE = "1,B001E4KFG0,A3SGXH7AUHU8GW,delmartian,1,1";
+    public static final String EMPTY_LINE = "";
+
+    @Autowired
+    private CsvLineParser<Review> parser;
+
+    @Test
+    public void testParser_OK() {
+        Review expected = Review.builder()
+                .id(ID)
+                .productId(PRODUCT_ID)
+                .userId(USER_ID)
+                .profileName(PROFILE_NAME)
+                .helpfulnessNumerator(HELPFULNESS_NUMERATOR)
+                .helpfulnessDenominator(HELPFULNESS_DENOMINATOR)
+                .score(SCORE)
+                .time(LocalDateTime.ofInstant(Instant
+                        .ofEpochSecond(SECONDS), ZoneId.systemDefault()))
+                .summary("Good Quality Dog Food")
+                .text("I have bought several of the Vitality canned dog food products and"
+                        + " have found them all to be of good quality. The product looks more"
+                        + " like a stew than a processed meat and it smells better. My Labrador"
+                        + " is finicky and she appreciates this product better than  most.")
+                .build();
+        Review actual = parser.parseData("1,B001E4KFG0,A3SGXH7AUHU8GW,delmartian,1,1,5,"
+                + "1303862400,Good Quality Dog Food,I have bought several of the Vitality canned"
+                + " dog food products and have found them all to be of good quality. The product"
+                + " looks more like a stew than a processed meat and it smells better. My Labrador"
+                + " is finicky and she appreciates this product better than  most.");
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void parseWrongFormatLine() {
+        parser.parseData(WRONG_FORMAT_LINE);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void parseEmptyLine() {
+        parser.parseData(EMPTY_LINE);
+    }
+}
